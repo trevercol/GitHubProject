9a3e86cadd7df6571be2c5f1adf3bff7b64b9606
Caysie98 <caysie98@gmail.com>
2015-04-02 13:19:52 -0500

    getMessages() and getUsers() completed

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,32 @@
+import java.util.*;
 
-public class BackEnd {
 
+public class BackEnd
+{
+	private TreeMap<String, List<String>> messages;
+	
+	public BackEnd()
+	{
+		
+	}
+	
+	public List<String> getMessages(String username)
+	{
+		return messages.get(username);
+	}
+	public void addMessage(String username, String msg)
+	{
+		
+	}
+	
+	public List<String> getUsers()
+	{
+		Set<String> keys = messages.keySet();
+		List<String> keyList = new ArrayList<String>();
+		for (String x: keys)
+		{
+			keyList.add(x);
+		}
+		return keyList;
+	}
 }
