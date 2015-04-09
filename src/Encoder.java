4863808159e4f9ebd88ea22087470657890bc9c5
trevercol <trevercol@gmail.com>
2015-04-09 13:31:56 -0500

    FrontEnd/BackEnd done, Encoder (work in progress)

--- src/Encoder.java
+++ src/Encoder.java
@@ -14,34 +14,13 @@ public class Encoder
 	private String plaintext;
 	
 	public static String rot13(String value) {
-
+		String alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 		char[] values = value.toCharArray();
-		for (int i = 0; i < values.length; i++) {
-			   char letter = values[i];
-
-			    
-			    if (letter >= 'a' && letter <= 'z') {
-				// Rotate lowercase letters.
-
-			    	if (letter > 'm') {
-			    		letter -= 13;
-			    			} 	
-			    	else {
-				    	letter += 13;
-						 }
-			    } 	else if (letter >= 'A' && letter <= 'Z') {
-				// Rotate uppercase letters.
-
-			    	if (letter > 'M') {
-			    		letter -= 13;
-				} 	else 
-					{
-				    	letter += 13;
-					}
-			    }
-			    		values[i] = letter;
-			}
-			return new String(values);
+		String toret = "";
+		for (char i:values) {
+			   if (alpha.contains(""+i))
+		}
+			return new String(toret);
 		}
 	public Encoder(String filename) 
 	{
